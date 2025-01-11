4cf075b0ffdd3b943471c93cc4c20198c02ed63e
kapil Narwani <124026841+kapil7071@users.noreply.github.com>
2025-01-11 22:37:13 +0530

    Update Config.java

--- taskmanagement2/src/main/java/com/w3villa/taskmanagement2/security/Config.java
+++ taskmanagement2/src/main/java/com/w3villa/taskmanagement2/security/Config.java
@@ -1,6 +1,5 @@
 package com.w3villa.taskmanagement2.security;
 
-import org.apache.catalina.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,43 +15,39 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class Config {
-  @Autowired
-  private UserDetailsService userDetailsService;
-  @Autowired
-  public Config(CustomUserDetailsService userDetailsService) {
-	  this.userDetailsService = userDetailsService;
-  }
-  @Bean
-  public UserDetailsService getUserDetailsService() {
-      return new CustomUserDetailsService();
-  }
+    private final UserDetailsService userDetailsService;
 
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-  }
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-      DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-      daoAuthenticationProvider.setUserDetailsService(getUserDetailsService());
-      daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-      return daoAuthenticationProvider;
-  }
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-      http.csrf(AbstractHttpConfigurer::disable)
-              .authorizeHttpRequests(authorize -> authorize
-                      .requestMatchers("/tasks/**").authenticated()
-                      .requestMatchers("/signin").permitAll()
-                      .requestMatchers("/signup").permitAll()
-                      .anyRequest().authenticated()
-              )
-              .formLogin(form -> form
-                      .loginPage("/signin")
-                      .loginProcessingUrl("/login")
-              ).logout(logout -> logout.logoutSuccessUrl("/signin"));
+    @Autowired
+    public Config(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
-      return http.build();
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/tasks/**").authenticated()
+                        .requestMatchers("/signin", "/signup", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/signin")
+                        .loginProcessingUrl("/login")
+                ).logout(logout -> logout.logoutSuccessUrl("/signin"));
+
+        return http.build();
+    }
 }
